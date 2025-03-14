dd7d18dc158304d12c55cfc5727441ad925e2f8a
Cristian Dragan <cristiandragan34@yahoo.ro>
2025-03-14 21:24:33 +0200

    Implemented the scanner

---
+++ src/navigation/screens/app/ScanScreen.tsx
@@ -0,0 +1,8 @@
+import {KContainer} from "../../../components";
+import {View} from "react-native";
+
+export const ScanScreen = () => {
+    return <KContainer>
+        <View></View>
+    </KContainer>
+}
