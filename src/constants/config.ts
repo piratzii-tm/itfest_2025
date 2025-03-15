fdcac9e8d49710947e4ba3405984ed447357fbc1
Delinschii Vlad <delinschiivlad@Delinschiis-MacBook-Pro.local>
2025-03-15 03:26:26 +0200

    Update gitignore
    Took 45 seconds

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth"
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { environment } from "./environment";
-
 
 const app = initializeApp(environment.firebaseConfig);
 
-export const auth =  initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-export const database = getDatabase(app)
+export const database = getDatabase(app);
