d3aa482365949fb11e4c97b11c1dbf70ca93fca8
Cristian Dragan <cristiandragan34@yahoo.ro>
2025-03-15 11:58:54 +0200

    Implemented room screen

--- src/navigation/screens/app/RoomScreen.tsx
+++ src/navigation/screens/app/RoomScreen.tsx
@@ -1,11 +1,48 @@
 import { KContainer } from "../../../components";
-import { View, Text } from "react-native-ui-lib";
+import { Colors, View, Text } from "react-native-ui-lib";
+import { Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { StackParamList } from "./HomeScreen";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import Svg, { Path } from "react-native-svg";
+import { useState } from "react";
+import { KLobbyControls } from "../../../components/KLobbyControls";
+import { KEdgeSvg } from "../../../components/KEdgeSvg";
+import { KSittingInfo } from "../../../components/KSittingInfo";
+import { KProduct } from "../../../components/KProduct";
 
 export const RoomScreen = () => {
+  const [restaurantName, setRestaurantName] = useState("Gourment Coffe");
+  const [date, setDate] = useState("Sept 4, 2024");
+  const [hour, setHour] = useState("8:00");
+
   return (
     <KContainer>
-      <View>
-        <Text>RoomSCreen</Text>
+      <KLobbyControls />
+      <View marginH-10>
+        <KEdgeSvg />
+        <View
+          style={{
+            backgroundColor: "#fff",
+            marginTop: -15,
+            alignItems: "center",
+          }}
+        >
+          <KSittingInfo
+            restaurantName={restaurantName}
+            date={date}
+            hour={hour}
+          />
+          <View paddingH-15 paddingV-10 width={"100%"} gap-10>
+            <KProduct
+              productName={"Coffe"}
+              productPrice={5}
+              productQuantity={2}
+            />
+          </View>
+        </View>
       </View>
     </KContainer>
   );
--- src/navigation/screens/app/tabs/HomeScreen.tsx
+++ src/navigation/screens/app/tabs/HomeScreen.tsx
@@ -1,12 +1,19 @@
-import {KContainer} from "../../../../components";
-import {View} from "react-native";
-import {Text} from "react-native-ui-lib";
+import { KContainer } from "../../../../components";
+import { Button, View } from "react-native";
+import { Text } from "react-native-ui-lib";
+import { useNavigation } from "@react-navigation/native";
 
 export const HomeScreen = () => {
-    return (
-        <KContainer>
-            <View>
-            </View>
-        </KContainer>
-    );
-}
+  const navigation = useNavigation();
+
+  return (
+    <KContainer>
+      <View>
+        <Button
+          title={"go to room"}
+          onPress={() => navigation.navigate("RoomScreen")}
+        />
+      </View>
+    </KContainer>
+  );
+};
